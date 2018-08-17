d21a3def1bb17a8feb293bb7c0c5265e9ace8bce
stephenlow <steve.a.low@gmail.com>
2018-08-17 17:46:39 -0400

    Remove unused imports

--- static/src/index.js
+++ static/src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import NavBanner from "../src/components/NavBanner";
-import Table from "../src/components/Table";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
