124e29f77df12bd29345b6add0ed3e2130a95c6c
stephenlow <steve.a.low@gmail.com>
2018-08-25 15:23:22 -0700

    Wire add song generic button and modal to python song create API endpoint

--- static/src/components/GenericButton.jsx
+++ static/src/components/GenericButton.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Modal from "react-modal";
+import Request from "superagent";
 
 const customStyles = {
   content: {
@@ -18,12 +19,16 @@ class GenericButton extends Component {
   constructor() {
     super();
     this.state = {
-      modalIsOpen: false
+      modalIsOpen: false,
+      name: "",
+      artist_name: "",
+      description: ""
     };
 
     this.openModal = this.openModal.bind(this);
     this.afterOpenModal = this.afterOpenModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
+    this.handleAddClick = this.handleAddClick.bind(this);
   }
 
   openModal() {
@@ -39,36 +44,76 @@ class GenericButton extends Component {
     this.setState({ modalIsOpen: false });
   }
 
-  handleClick = () => {
-    console.log("this is:", this);
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
   };
 
+  handleAddClick(event) {
+    event.preventDefault();
+    const { name, artist_name, description } = this.state;
+    const API_POST_SONGS = "http://localhost:5000/song/add";
+    let request =
+      '{ "data":{ "type": "song", "attributes": { "name":' +
+      JSON.stringify(name) +
+      ', "artist_name": ' +
+      JSON.stringify(artist_name) +
+      ', "description": ' +
+      JSON.stringify(description) +
+      "}}}";
+
+    console.log(request);
+    Request.post(API_POST_SONGS)
+      .set("Content-Type", "application/json")
+      .send(request)
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
+
+    this.closeModal();
+    window.location.reload();
+  }
+
   render() {
+    const { modalIsOpen, name, artist_name, description } = this.state;
     return (
       <div>
         <button onClick={this.openModal}>+ Add Song</button>
         <Modal
-          isOpen={this.state.modalIsOpen}
+          isOpen={modalIsOpen}
           onAfterOpen={this.afterOpenModal}
           onRequestClose={this.closeModal}
           style={customStyles}
-          contentLabel="Example Modal"
+          contentLabel="Add Song Modal"
         >
           <h2 ref={subtitle => (this.subtitle = subtitle)}>Add a new song:</h2>
-          <form>
+          <form onSubmit={this.handleAddClick}>
             <label>
               Name:
-              <input type="text" name="name" />
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={this.onChange}
+              />
             </label>
             <br />
             <label>
               Artist Name:
-              <input type="text" name="artist_name" />
+              <input
+                type="text"
+                name="artist_name"
+                value={artist_name}
+                onChange={this.onChange}
+              />
             </label>
             <br />
             <label>
               Description:
-              <input type="text" name="description" />
+              <input
+                type="text"
+                name="description"
+                value={description}
+                onChange={this.onChange}
+              />
             </label>
             <br />
             <button onClick={this.closeModal}>Close</button>
